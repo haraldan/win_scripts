06a1e18513f84f11a0a91d97dfd314cab33eeb99
Daniel Sinev <daniel.sinev@gmail.com>
2024-11-03 11:52:24 +0100

    remove Powershell profile

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,4 +0,0 @@
-Set-Alias -Name vim -Value nvim
-function ssh-ds {ssh ds@jarnold649-p350 -t 'env TERM=screen-256color tmux attach || env TERM=screen-256color tmux'}
-function wsl-default {wsl ~ tmux new -A -s default}
-function wsl-cmd {wsl ~ tmux new -A -s cmd}
